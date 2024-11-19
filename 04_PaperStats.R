9ddc5f1da87efc5cbb32e64d27b848eeebb44d63
hdugan <hilarydugan@gmail.com>
2024-11-19 17:06:51 -0600

    paper stats

--- 04_PaperStats.R
+++ 04_PaperStats.R
@@ -1,7 +1,7 @@
 library(synchrony)
 
 # Impact on melting ice (adding water) versus warming water column based on Lake Fryxell 
-a = df.full.ice |> filter(location_name == 'Lake Fryxell' & date_time == as.Date('2023-11-13'))
+a = hypo.join |> filter(location_name == 'Lake Fryxell' & date_time == as.Date('2023-11-13'))
 # current heat content
 a |> 
   summarise(ice.approx = mean(ice.approx, na.rm = T), heat_J = sum(heat_J, na.rm = T), heatIce_J = sum(heatIce_J, na.rm = T), 
@@ -13,7 +13,7 @@ a |>
   mutate(tempUse = if_else(depth.asl >= 14.7 & depth.asl <= 15.7, 0, tempUse)) |>
   mutate(iceDensity_kgm3 = if_else(depth.asl >= 14.7 & depth.asl <= 15.7, NA, iceDensity_kgm3)) |>
   
-  mutate(temp_K = tempUse + 273.15) %>%
+  mutate(temp_K = tempUse + 5) %>%
   mutate(spHeat_J_m3K = spHeat_J_kgK * density_kg_m3) %>% 
   # latent heat of ice = density * thickness *  latent heat of ice (334000 J/kg)
   mutate(LHice_J_m3 = iceDensity_kgm3 * 334000) |> 
@@ -28,7 +28,7 @@ a |>
 a |> 
   mutate(tempUse = tempUse + 5) |>
   # mutate(tempUse = if_else(depth.asl >= 14.7 & depth.asl <= 15.7, 0, tempUse)) |>
-  mutate(temp_K = tempUse + 273.15) %>%
+  mutate(temp_K = tempUse + 5) %>%
   mutate(spHeat_J_m3K = spHeat_J_kgK * density_kg_m3) %>% 
   # latent heat of ice = density * thickness *  latent heat of ice (334000 J/kg)
   mutate(LHice_J_m3 = iceDensity_kgm3 * 334000) |> 
