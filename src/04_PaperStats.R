34f06dbb15d38523ec7164df1164cd5089f3ad10
hdugan <hilarydugan@gmail.com>
2024-11-25 13:52:40 -0600

    updated paper stats

--- src/04_PaperStats.R
+++ src/04_PaperStats.R
@@ -23,16 +23,16 @@ a |>
             Area_2D = first(Area_2D)) |> 
   mutate(heatTot_J_m2 = (heat_J - heatIce_J)/Area_2D/1e6)
 
-# If you warmed the water column 1°C
+# If you warmed the water column 2°C
 a |> 
-  mutate(tempUse = tempUse + 1) |>
+  mutate(tempUse = tempUse + 2) |>
   # mutate(tempUse = if_else(depth.asl >= 14.7 & depth.asl <= 15.7, 0, tempUse)) |>
-  mutate(temp_K = tempUse + 5) %>%
+  mutate(temp_FPD = tempUse - FPD) %>% # set baseline temperature to -5°C
   mutate(spHeat_J_m3K = spHeat_J_kgK * density_kg_m3) %>% 
   # latent heat of ice = density * thickness *  latent heat of ice (334000 J/kg)
   mutate(LHice_J_m3 = iceDensity_kgm3 * 334000) |> 
   mutate(heatIce_J = LHice_J_m3 * vol_layer_m3) |> 
-  mutate(heat_J = spHeat_J_m3K * vol_layer_m3 * temp_K) %>% 
+  mutate(heat_J = spHeat_J_m3K * vol_layer_m3 * temp_FPD) %>% 
   mutate(heat_J_m2 = heat_J/Area_2D) |> 
   summarise(ice.approx = mean(ice.approx, na.rm = T), heat_J = sum(heat_J, na.rm = T), heatIce_J = sum(heatIce_J, na.rm = T), 
             Area_2D = first(Area_2D)) |> 
